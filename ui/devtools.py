f1392b15d55ee378f6635c6c83d64d26eeae3927
ArnabRollin <iamarnab.phukan@gmail.com>
2022-12-03 11:12:20 +0530

    Updated DevTools

--- ui/devtools.py
+++ ui/devtools.py
@@ -10,7 +10,14 @@ def join_geometry(width: int, height: int, x: int = 0, y: int = 0) -> str:
     return f"{width}x{height}+{x}+{y}"
 
 
-def span_list(list_: list[_Any, _Any]) -> str:
+# @deprecated
+def span_list_deprecated(list_: list[_Any]) -> str:
     str_list = str(list_)
     returner = str_list.replace("'", "")
+    returner = str_list.replace("\"", "")
     return returner[1:len(returner)-1]
+
+
+def span_list(list_: list[_Any]) -> str:
+    str_list = str(list_)
+    return str_list[1:len(str_list)-1]
